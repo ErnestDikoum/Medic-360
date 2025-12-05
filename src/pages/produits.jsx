2ed0bf0667964d8f9d5c1b7ee22e885c62fdeb90
Kaneki <ernestito.dikoum@gmail.com>
2025-12-05 05:47:46 +0100

    update

--- src/pages/produits.jsx
+++ src/pages/produits.jsx
@@ -41,7 +41,7 @@ const Produits = () => {
     <div className="min-h-screen bg-gray-50 py-10 px-4">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold text-center text-blue-700 mb-8">
-          Gestion des Produits
+          Gestion des Produits Pharmaceutique
         </h1>
 
         {/* Formulaire */}
