2e7f3450a6af61da8765176e7a941e993b22fcb8
Kaneki <ernestito.dikoum@gmail.com>
2025-12-06 03:00:56 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,4 @@
-import axios from 'axios';
-
 export const api = axios.create({
-  baseURL: 'https://medicb-360.onrender.com/api', // pointe directement sur le préfixe API
-  headers: {
-    'Content-Type': 'application/json', // toujours envoyer du JSON
-    'Accept': 'application/json',
-  },
-  withCredentials: false, // à mettre true si tu utilises des cookies/sessions Laravel
+  baseURL: 'https://medicb-360.onrender.com/api',
+  headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
 });
