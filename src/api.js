d9c8c9a9e764eb6739c301d57bffb07896bd5f03
Kaneki <ernestito.dikoum@gmail.com>
2025-12-07 10:50:43 +0100

    Update API base URL to remove '/api' suffix

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 export const api = axios.create({
-  baseURL: 'https://medicb-360.onrender.com/api',
+  baseURL: 'https://medicb-360.onrender.com',
   headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
 });
