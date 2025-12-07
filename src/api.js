fd9a62dc779cb138c779e0b37911e6d166420bf2
Kaneki <ernestito.dikoum@gmail.com>
2025-12-07 13:42:56 +0100

    Add axios import to api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const api = axios.create({
   baseURL: 'https://medicb-360.onrender.com',
   headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
